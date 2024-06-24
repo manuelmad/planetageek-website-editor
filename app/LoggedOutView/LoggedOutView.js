4736be9d83666ffbac28c49e6206d08e5f91ceed
Manuel Araujo <manuelmad1981@gmail.com>
2024-06-24 10:54:13 -0400

    Adding event to email input, when enter key pressed, to focus on next input (password). Adding event to password input, when enter key pressed, to focus on Log In button and loging.

--- app/LoggedOutView/LoggedOutView.js
+++ app/LoggedOutView/LoggedOutView.js
@@ -59,6 +59,18 @@ export default function LoggedOutView({
         input_password.type = 'password';
     }
 
+    const focusOnNextInput = (event) => {
+        if (event.keyCode == 13) {
+            document.querySelector('#user_password').focus();
+        }
+    }
+
+    const focusOnLogInButton = (event) => {
+        if (event.keyCode == 13) {
+            document.querySelector('#login_btn').focus();
+        }
+    }
+
     return (
         <section className="logged-out-container" style={displayLoggedOutView}>
             <article>
@@ -74,11 +86,11 @@ export default function LoggedOutView({
                 <div className="user-login-container">
                     <p><label htmlFor="user_email">E-mail:</label></p>
                     <p>
-                        <input type="email" id="user_email" placeholder="micorreoejemplo@gmail.com" autoFocus></input>
+                        <input type="email" id="user_email" placeholder="micorreoejemplo@gmail.com" autoFocus  onKeyDown={focusOnNextInput}></input>
                     </p>
                     <p><label htmlFor="user_password">Password:</label></p>
                     <p className="input-password-container">
-                        <input type="password" id="user_password"></input>
+                        <input type="password" id="user_password" onKeyDown={focusOnLogInButton}></input>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16" onClick={showPassword}>
                             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
                             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
